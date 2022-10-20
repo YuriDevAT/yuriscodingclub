088e9a08aa8c53bbfee58fa2d8695b2cfa559568
Julia Undeutsch <JUndeutsch@gmx.at>
2022-10-20 13:41:13 +0200

    add var in css; add style to articles and links

--- pages/get-a-job.tsx
+++ pages/get-a-job.tsx
@@ -31,13 +31,35 @@ const GetAJob = () => {
       </p>
       <h3>Personal performance</h3>
       <p>
-        I&apos;ve written a few articles on personal performance that you should definitely check out.These tips have helped other people get interviews as well that no recruiter had noticed before.
+        I&apos;ve written a few articles on personal performance that you should definitely check out. These tips have helped other people get interviews as well that no recruiter had noticed before.
       </p>
-      <ul>
-        <li>How to <a href="https://dev.to/yuridevat/how-to-write-a-promising-cv-32ih">write a promising CV</a></li>
-        <li>How to <a href="https://dev.to/yuridevat/how-to-create-a-stunning-github-profile-2mh5">create a stunning GitHub profile</a></li>
-        <li>How to <a href="https://dev.to/yuridevat/how-to-create-a-professional-linkedin-profile-2n11">create an professional LinkedIn Profile</a></li>
-      </ul>
+      <section className="articles">
+        <article className="article">
+          <a href="https://dev.to/yuridevat/how-to-write-a-promising-cv-32ih">
+            <h4>How to write a promising&nbsp;CV</h4>
+            <p>
+              Read how to create an awesome github profile and make an outstanding portfolio like many others.
+            </p>
+          </a>
+        </article>
+        <article className="article">
+          <a href="https://dev.to/yuridevat/how-to-create-a-stunning-github-profile-2mh5">
+            <h4>How to create a stunning GitHub&nbsp;profile</h4>
+            <p>
+              Read how to create an awesome github profile and make an outstanding portfolio like many others.
+            </p>
+          </a>
+        </article>
+        <article className="article">
+          <a href="https://dev.to/yuridevat/how-to-create-a-professional-linkedin-profile-2n11">
+            <h4>How to create an professional LinkedIn&nbsp;Profile</h4>
+            <p>
+              Read how to create an awesome github profile and make an outstanding portfolio like many others.
+            </p>
+          </a>
+        </article>
+      </section>
+
       <p>
         Take a look at <a href="https://github.com/YuriDevAT">my GitHub profile</a> as well to get an idea of what I&apos; m talking about.I didn&apos;t even had a portfolio but used my GitHub profile to show off my projects.
       </p>
@@ -51,7 +73,7 @@ const GetAJob = () => {
       <p>
         So here&apos;s a checklist, that you can work through for each of your projects and check that you&apos;ve considered these things at all. Of course, no project has to be perfect and no project will ever be finished. But it&apos;s still a great feeling to know that you gave it your all to get the most out of each project. If you follow these simple steps, you&apos;ll have a lot more to talk about in job interviews.
       </p>
-      <a href="https://github.com/YuriDevAT/job-seekers/wiki/Checklist-for-your-portfolio-projects">Grab the checklist</a>
+      <a className="linked-tag" href="https://github.com/YuriDevAT/job-seekers/wiki/Checklist-for-your-portfolio-projects">Grab the checklist</a>
       <h3>Interview preparation</h3>
       <p>
         It would be a good practice to keep all the information about the applied jobs in one file. That&apos;s where <a href="https://instinctive-cycle-28f.notion.site/Job-Applications-Template-3ecf0503f6a2401da865b29f72da58a7">Notion&apos;s Job Applications Template</a> comes in handy. You can add the companies you applied to and the job description to be up to date when the final call comes (each job description is different, so preparing for it is sometimes very different).
@@ -62,7 +84,7 @@ const GetAJob = () => {
       <p>
         Hopefully it is self-explanatory that you have stable internet during the interview, have code ready to show (GitHub, live view, and VSC), are in a quiet environment with appropriate background and clothing.
       </p>
-      <a href="https://github.com/YuriDevAT/job-seekers/wiki/Interview-Questions">Interview questions</a>
+      <a className="linked-tag" href="https://github.com/YuriDevAT/job-seekers/wiki/Interview-Questions">Interview questions</a>
     </>
   )
 }
