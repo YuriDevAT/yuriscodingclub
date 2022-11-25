c73333f37d2250e4548b11b0cc3de6b931095899
Julia Undeutsch <JUndeutsch@gmx.at>
2022-11-25 17:56:48 +0100

    hide header on home; adjust home

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,12 +2,16 @@ import { ReactNode } from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const isHome = router.pathname === '/';
+
   return (
     <>
       <a href="#main" className="skip-link" >Skip to main content</a>
-      <Header />
+      {!isHome && <Header />}
       <main className="main-container" id="main">
         {children}
       </main>
