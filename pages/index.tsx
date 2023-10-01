4114c6f066e79abfa946d240c8ec8ce2aecf711a
Julia Undeutsch <JUndeutsch@gmx.at>
2023-10-01 05:42:51 +0200

    change subheadline to "an"

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         Welcome to <br />
         <Image src="/images/YurisCodingClub.svg" width={650} height={150} alt={siteTitle} />
       </h1>
-      <h2>The most inclusive open source community!</h2>
+      <h2>An inclusive open source community!</h2>
       <p className="home__text">
         Are you ready to become an Frontend Developer focused on Accessibility?<br />
         Especially for <b>people with disabilities and women of all ages</b>, YurisCodingClub aims to be a community with which <span className="upper">you</span> can achieve your goals.
